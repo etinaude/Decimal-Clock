44c25f5e7cf18a0fa9e33bf0c92dd7cbe0ab7880
Etienne Naude <etinaude@gmail.com>
2019-09-18 22:16:50 +1200

    added button interaction

--- arduino/dec/dec.ino
+++ arduino/dec/dec.ino
@@ -1,9 +1,11 @@
-/*==============================================================================*
-* please read the README.md file                                                *
-*                                                                               *
-* this file is for the decimal time arduino clock                               *
-*                                                                               *
-*===============================================================================*/
+/*=====================================================*
+* please read the README.md file                       *
+*                                                      *
+* this file is for the decimal time arduino clock     *
+* loops for a 10hr day                                 *
+*                                                      *
+*======================================================*/
+//imports libraries--------------------------------
 #include <Adafruit_SSD1306.h>
 #include <splash.h>
 #include <Adafruit_GFX.h>
@@ -14,40 +16,124 @@
 Adafruit_SSD1306 display(OLED_RESET);
 #define LOGO16_GLCD_HEIGHT 16 
 #define LOGO16_GLCD_WIDTH  16
-//reset defaults
+
+//reset defaults---------------------------------------
 const int hr=0;
 const int mins=0;
 const int sec=0;
-const int button=3;
-int buttonState = 0;
-//declare counters
+
+//set up buttons---------------------------------------
+const int mode_button=3;
+int mode_buttonState = 0;
+const int up_button=2;
+int up_buttonState = 0;
+const int down_button=1;
+int down_buttonState = 0;
+
+//declare counters-------------------------------------
 int dec_sec=sec;int dec_min=mins;int dec_hr=hr;
 int start= millis();int dec_start= millis();int current= millis();
 String dec_str ="";
+
+//initalization----------------------------------------
 void setup() {
     Serial.begin(9600);
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     display.clearDisplay();
     display.setTextColor(WHITE);
-    pinMode(button,INPUT);
+    
+    //set buttons---------------------------------------
+    pinMode(mode_button,INPUT);
+    pinMode(up_button,INPUT);
+    pinMode(down_button,INPUT);
+
+    //displays starter text------------------------------
     display.setTextSize(1);
     display.println();
     display.setTextSize(2);
     display.println("  DECIMAL");
     display.display();
-    start= millis();dec_start= millis();current= millis();
     delay(1000);
+    start= millis();dec_start= millis();current= millis();
 }
-void but(){
-    buttonState = digitalRead(button);
-    if(buttonState == HIGH){
-        dec_hr=hr;
-        dec_min =mins;
-        dec_sec=sec;
-        dec_start = millis();
+
+//resets time-------------------------------------------
+void reset(){
+    mode_buttonState = digitalRead(mode_button);
+    dec_hr=hr;
+    dec_min =mins;
+    dec_sec=sec;
+    dec_start = millis();
+}
+//------------------------------------------------------
+
+//handle buttons----------------------------------------
+void change_sec(){
+  while(mode_buttonState == LOW){
+      up_buttonState = digitalRead(up_button);
+      down_buttonState = digitalRead(down_button);
+      mode_buttonState = digitalRead(mode_button);
+    if(up_buttonState ==HIGH || down_buttonState ==HIGH){
+      dec_sec =0;
     }
+    up_buttonState = digitalRead(up_button);
+    down_buttonState = digitalRead(down_button);
+    mode_buttonState = digitalRead(mode_button);
+  }
 }
+
+void change_min(){
+      up_buttonState = digitalRead(up_button);
+      down_buttonState = digitalRead(down_button);
+      mode_buttonState = digitalRead(mode_button);
+  while(mode_buttonState == LOW){
+    if(up_buttonState ==HIGH){
+      dec_min++;
+    }
+    if(down_buttonState == HIGH){
+      dec_min--;
+    }
+    up_buttonState = digitalRead(up_button);
+    down_buttonState = digitalRead(down_button);
+    mode_buttonState = digitalRead(mode_button);
+  }
+}
+
+void change_hr(){
+  up_buttonState = digitalRead(up_button);
+  down_buttonState = digitalRead(down_button);
+  mode_buttonState = digitalRead(mode_button);
+  while(mode_buttonState == LOW){
+    if(up_buttonState ==HIGH){
+      dec_hr++;
+    }
+    if(down_buttonState == HIGH){
+      dec_hr--;
+    }
+      up_buttonState = digitalRead(up_button);
+      down_buttonState = digitalRead(down_button);
+      mode_buttonState = digitalRead(mode_button);
+  }
+}
+
+void but(){
+  mode_buttonState = digitalRead(mode_button);
+  if(mode_buttonState == HIGH){
+    change_sec();
+    change_min();
+    change_hr();
+  }
+}
+//-----------------------------------------------------
+
+//handle display---------------------------------------
 void disp(){
+    //formats output text------------------------------
+    if(dec_min<10){dec_str = String(dec_hr)+":0"+String(dec_min);
+    }else{dec_str = String(dec_hr)+":"+String(dec_min);}
+    if(dec_sec<10){dec_str = dec_str+":0"+String(dec_sec);
+    }else{dec_str = dec_str = dec_str+":"+String(dec_sec);}
+    //displays text------------------------------------
     display.setTextSize(1);
     display.setCursor(10,0);
     display.clearDisplay();
@@ -56,6 +142,9 @@ void disp(){
     display.println("  "+dec_str);
     display.display();
 }
+//------------------------------------------------------
+
+//increments time---------------------------------------
 void incr(){
     if(current-dec_start >=863){
         dec_start = millis();
@@ -67,21 +156,17 @@ void incr(){
           }else{dec_sec++;}
     }
 }
-void form(){
-  if(dec_min<10){dec_str = String(dec_hr)+":0"+String(dec_min);
-  }else{dec_str = String(dec_hr)+":"+String(dec_min);}
-  if(dec_sec<10){dec_str = dec_str+":0"+String(dec_sec);
-  }else{dec_str = dec_str = dec_str+":"+String(dec_sec);}   
-}
+//-----------------------------------------------------
+
+//loops forever----------------------------------------
 void loop() {
-    //set current time
+    //set current time---------------------------------
     current = millis();
-    //increment decimal time
+    //increment decimal time---------------------------
     incr();
-    //formats text
-    form();
-    //displays standard time
+    //displays standard time---------------------------
     disp();
-    //test to see if the button is pressed
+    //test to see if the button is pressed-------------
     but();
 }
+//-----------------------------------------------------
--- arduino/std/std.ino
+++ arduino/std/std.ino
@@ -1,9 +1,11 @@
-/*==============================================================================*
-* please read the README.md file                                                *
-*                                                                               *
-* this file is for the standard time arduino clock                              *
-*                                                                               *
-*===============================================================================*/
+/*=====================================================*
+* please read the README.md file                       *
+*                                                      *
+* this file is for the standard time arduino clock     *
+* loops for a 24hr day                                 *
+*                                                      *
+*======================================================*/
+//imports libraries--------------------------------
 #include <Adafruit_SSD1306.h>
 #include <splash.h>
 #include <Adafruit_GFX.h>
@@ -14,22 +16,38 @@
 Adafruit_SSD1306 display(OLED_RESET);
 #define LOGO16_GLCD_HEIGHT 16 
 #define LOGO16_GLCD_WIDTH  16
-//reset defaults
+
+//reset defaults---------------------------------------
 const int hr=0;
 const int mins=0;
 const int sec=0;
-const int button=3;
-int buttonState = 0;
-//declare counters
+
+//set up buttons---------------------------------------
+const int mode_button=3;
+int mode_buttonState = 0;
+const int up_button=2;
+int up_buttonState = 0;
+const int down_button=1;
+int down_buttonState = 0;
+
+//declare counters-------------------------------------
 int std_sec=0;int std_min=0;int std_hr=0;int dec_hr=0;
 int start= millis();int std_start= millis();int current= millis();
 String std_str ="";
+
+//initalization----------------------------------------
 void setup() {
   Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextColor(WHITE);
-  pinMode(button,INPUT);
+
+  //set buttons----------------------------------------
+  pinMode(mode_button,INPUT);
+  pinMode(up_button,INPUT);
+  pinMode(down_button,INPUT);
+
+  //displays starter text------------------------------
   display.setTextSize(1);
   display.println();
   display.setTextSize(2);
@@ -38,16 +56,84 @@ void setup() {
   delay(1000);
   start= millis();std_start= millis();current= millis(); 
 }
-void but(){
-  buttonState = digitalRead(button);
-  if(buttonState == HIGH){
+
+//resets time-------------------------------------------
+void reset(){
+    mode_buttonState = digitalRead(mode_button);
     std_hr=hr;
     std_min =mins;
     std_sec=sec;
     std_start = millis();
+}
+//------------------------------------------------------
+
+//handle buttons----------------------------------------
+void change_sec(){
+  while(mode_buttonState == LOW){
+      up_buttonState = digitalRead(up_button);
+      down_buttonState = digitalRead(down_button);
+      mode_buttonState = digitalRead(mode_button);
+    if(up_buttonState ==HIGH || down_buttonState ==HIGH){
+      std_sec =0;
+    }
+    up_buttonState = digitalRead(up_button);
+    down_buttonState = digitalRead(down_button);
+    mode_buttonState = digitalRead(mode_button);
   }
 }
+
+void change_min(){
+      up_buttonState = digitalRead(up_button);
+      down_buttonState = digitalRead(down_button);
+      mode_buttonState = digitalRead(mode_button);
+  while(mode_buttonState == LOW){
+    if(up_buttonState ==HIGH){
+      std_min++;
+    }
+    if(down_buttonState == HIGH){
+      std_min--;
+    }
+    up_buttonState = digitalRead(up_button);
+    down_buttonState = digitalRead(down_button);
+    mode_buttonState = digitalRead(mode_button);
+  }
+}
+
+void change_hr(){
+  up_buttonState = digitalRead(up_button);
+  down_buttonState = digitalRead(down_button);
+  mode_buttonState = digitalRead(mode_button);
+  while(mode_buttonState == LOW){
+    if(up_buttonState ==HIGH){
+      std_hr++;
+    }
+    if(down_buttonState == HIGH){
+      std_hr--;
+    }
+      up_buttonState = digitalRead(up_button);
+      down_buttonState = digitalRead(down_button);
+      mode_buttonState = digitalRead(mode_button);
+  }
+}
+
+void but(){
+  mode_buttonState = digitalRead(mode_button);
+  if(mode_buttonState == HIGH){
+    change_sec();
+    change_min();
+    change_hr();
+  }
+}
+//-----------------------------------------------------
+
+//handle display---------------------------------------
 void disp(){
+  //formats output text------------------------------
+  if(std_min<10){std_str = String(std_hr)+":0"+String(std_min);
+  }else{std_str = String(std_hr)+":"+String(std_min);}
+  if(std_sec<10){std_str = std_str+":0"+String(std_sec);
+  }else{std_str = std_str = std_str+":"+String(std_sec);}
+  //displays text------------------------------------
   display.setTextSize(1);
   display.setCursor(10,0);
   display.clearDisplay();
@@ -56,6 +142,9 @@ void disp(){
   display.println("  "+std_str);
   display.display();
 }
+//------------------------------------------------------
+
+//increments time---------------------------------------
 void incr(){
   if(current-std_start >=1000){
     std_start = millis();
@@ -67,21 +156,17 @@ void incr(){
     }else{std_sec++;}
   }
 }
-void form(){
-  if(std_min<10){std_str = String(std_hr)+":0"+String(std_min);
-  }else{std_str = String(std_hr)+":"+String(std_min);}
-  if(std_sec<10){std_str = std_str+":0"+String(std_sec);
-  }else{std_str = std_str = std_str+":"+String(std_sec);}   
-}
-void loop() {
-  //set current time
+//-----------------------------------------------------
+
+//loops forever----------------------------------------
+void loop(){
+  //set current time-----------------------------------
   current = millis();
-  //increment standard time
+  //increment decimal time-----------------------------
   incr();
-  //formats text
-  form();
-  //displays standard time
+  //displays standard time-----------------------------
   disp();
-  //test to see if the button is pressed
+  //test to see if the button is pressed---------------
   but();
 }
+//-----------------------------------------------------
